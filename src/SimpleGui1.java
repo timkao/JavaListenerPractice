96f06f9311b493443bc094c91b79e64aa5194bb3
Tim Kao <pakuya.kao@gmail.com>
2019-09-22 11:21:01 -0700

    add first listener to the button

--- src/SimpleGui1.java
+++ src/SimpleGui1.java
@@ -1,14 +1,29 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SimpleGui1 {
+public class SimpleGui1 implements ActionListener {
+    JButton button;
+
     public static void main(String[] args) {
+        SimpleGui1 gui = new SimpleGui1();
+        gui.go();
+    }
+
+    public void go() {
         JFrame frame = new JFrame();
-        JButton button = new JButton("click me");
+        button = new JButton("Click me");
+        button.addActionListener(this);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(button);
         frame.setSize(300, 300);
         frame.setVisible(true);
+
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        button.setText("I am clicked");
+    }
 }
