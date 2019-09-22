2461f344f937c061baee0eb274e58886717136a0
Tim Kao <pakuya.kao@gmail.com>
2019-09-22 13:13:58 -0700

    more refactoring

--- src/SimpleGui1.java
+++ src/SimpleGui1.java
@@ -20,10 +20,20 @@ public class SimpleGui1 {
         label = new JLabel("This is the label");
 
         panelButton = new JButton("Paint it randomly");
-        panelButton.addActionListener(new PanelActionListener());
+        panelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panel.repaint();
+            }
+        });
 
         labelButton = new JButton("Change Text");
-        labelButton.addActionListener(new LabelActionListener());
+        labelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText("Changed it");
+            }
+        });
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(BorderLayout.SOUTH, panelButton);
@@ -33,19 +43,4 @@ public class SimpleGui1 {
         frame.setSize(300, 300);
         frame.setVisible(true);
     }
-
-    class PanelActionListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            panel.repaint();
-        }
-    }
-
-    class LabelActionListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            label.setText("label changed");
-        }
-    }
-
 }
